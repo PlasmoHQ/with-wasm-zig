e2b429d1e3da761e6180175f319f215abf558d18
Louis <6723574+louisgv@users.noreply.github.com>
2022-12-09 21:09:14 -0400

    Test again

--- src/popup.tsx
+++ src/popup.tsx
@@ -19,7 +19,7 @@ function IndexPopup() {
         flexDirection: "column",
         padding: 16
       }}>
-      <h1>Zig Calculator MK119800</h1>
+      <h1>Zig Calculator MK33800</h1>
       <input
         maxLength={8}
         value={`A = ${a}`}
